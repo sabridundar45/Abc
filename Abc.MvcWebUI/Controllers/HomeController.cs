d2d8e11de1552c56a0fb7dd7aec4baa19ef83249
sabridundar45 <sabri_dundar@yahoo.com>
2017-10-15 20:51:41 +0300

    Update HomeController.cs

--- Abc.MvcWebUI/Controllers/HomeController.cs
+++ Abc.MvcWebUI/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace Abc.MvcWebUI.Controllers
 
         public ActionResult Details(int id)
         {
-            return View(_context.Products.Where(i => i.Id == id).FirstOrDefault());
+            return View(_context.Products.FirstOrDefault(i => i.Id == id));
         }
 
         public ActionResult List()
